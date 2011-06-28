1252ccd3f420036a6740c761c174ca77bfe517d6
Martin Maier <tine2k@imac.(none)>
2011-06-28 19:01:15 +0200

    Initial Commit

---
+++ Reiseabrechnung/ReiseabrechnungAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ReiseabrechnungAppDelegate.h
+//  Reiseabrechnung
+//
+//  Created by Martin Maier on 28/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReiseabrechnungAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Reiseabrechnung/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Reiseabrechnung
+//
+//  Created by Martin Maier on 28/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
