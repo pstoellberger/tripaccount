9ddf339d728f59e2e667a32dbf23881aff2be0c4
Martin Maier <tine2k@imac.(none)>
2011-08-01 08:36:25 +0200

    updated image size issue

---
+++ Reiseabrechnung/ParticipantHelperCategory.m
@@ -0,0 +1,61 @@
+//
+//  ParticipantHelper.m
+//  Reiseabrechnung
+//
+//  Created by Martin Maier on 20/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ParticipantHelperCategory.h"
+#import "Travel.h"
+
+@implementation Participant (ParticipantHelper)
+
++ (BOOL)addParticipant:(Participant *)person toTravel:(Travel *)travel withABRecord:(ABRecordRef)recordRef {
+    
+    NSString *firstName = (NSString *) ABRecordCopyValue(recordRef, kABPersonFirstNameProperty);
+    NSString *lastName = (NSString *) ABRecordCopyValue(recordRef, kABPersonLastNameProperty);
+    NSString *fullName = [NSString stringWithFormat:@"%@ %@", firstName, lastName];
+    
+    BOOL addPerson = YES;
+    
+    for(Participant *p in travel.participants) {
+        if ([p.name isEqualToString:fullName]) {
+            addPerson= NO;
+            break;
+        }
+    }
+
+    if (addPerson) {
+        person.travel = travel;
+        person.name = fullName;
+        
+        UIImage *image = nil;
+        if(ABPersonHasImageData(recordRef)) {
+            
+            NSData *imgData = nil;
+            if (&ABPersonCopyImageDataWithFormat != nil) {
+                // iOS >= 4.1
+                imgData = (NSData *) ABPersonCopyImageDataWithFormat(recordRef, kABPersonImageFormatThumbnail);
+            } else {
+                // iOS < 4.1
+                imgData = (NSData *) ABPersonCopyImageData(recordRef);
+            }
+            
+            image = [UIImage imageWithData:imgData];
+            if (image) {
+                person.image = UIImagePNGRepresentation(image);
+            }
+            [imgData release];
+        } else {
+            person.image = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"noImage" ofType:@"png"]];
+        }
+    }
+    
+    [firstName release];
+    [lastName release];
+    
+    return addPerson;
+}
+
+@end
