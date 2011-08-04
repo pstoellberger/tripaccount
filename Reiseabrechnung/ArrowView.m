5159671af19bcc927bfc5f4fd2c9ba675648cf68
Martin Maier <martinmaier@MacBook-Pro.local>
2011-08-04 19:32:12 +0200

    move to proper dir

---
+++ Reiseabrechnung/ArrowView.m
@@ -0,0 +1,46 @@
+//
+//  ArrowView.m
+//  Reiseabrechnung
+//
+//  Created by Martin Maier on 27/07/2011.
+//  Copyright 2011 Martin Maier. All rights reserved.
+//
+
+#import "ArrowView.h"
+
+@implementation ArrowView
+
+@synthesize upsideDown=_upsideDown;
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    if ((self = [super initWithFrame:frame])) {
+        self.backgroundColor = [UIColor clearColor];
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect {
+    
+    [super drawRect:rect];
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextBeginPath(context);
+    
+    if (!self.upsideDown) {
+        CGContextMoveToPoint(context , 0, rect.size.height);
+        CGContextAddLineToPoint(context , rect.size.width, rect.size.height);
+        CGContextAddLineToPoint(context , rect.size.width / 2, 0 );
+    } else {
+        CGContextMoveToPoint(context , 0, 0);
+        CGContextAddLineToPoint(context , rect.size.width, 0);
+        CGContextAddLineToPoint(context , rect.size.width / 2, rect.size.height);        
+    }
+    CGContextClosePath(context);
+    
+    CGContextDrawPath(context, kCGPathFill);
+}
+
+@end
