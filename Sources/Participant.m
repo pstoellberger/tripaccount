3b8043ab6fb0e71ebdca8847f5160be9aecded77
Martin Maier <tine2k@imac.(none)>
2011-11-15 20:44:14 +0100

    to fix M marker

--- Sources/Participant.m
+++ Sources/Participant.m
@@ -2,7 +2,7 @@
 //  Participant.m
 //  Reiseabrechnung
 //
-//  Created by Martin Maier on 01/11/2011.
+//  Created by Martin Maier on 15/11/2011.
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
