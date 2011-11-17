08c84cfa6528254431a3fbf8c3434fc35c466ce2
Martin Maier <tine2k@imac.(none)>
2011-11-17 22:37:15 +0100

    fixed (null)

--- Sources/ParticipantHelperCategory.m
+++ Sources/ParticipantHelperCategory.m
@@ -19,62 +19,74 @@
 
 + (BOOL)addParticipant:(Participant *)person toTravel:(Travel *)travel withABRecord:(ABRecordRef)recordRef andEmail:(NSString *)email {
     
-    NSString *firstName = (NSString *) ABRecordCopyValue(recordRef, kABPersonFirstNameProperty);
-    NSString *lastName = (NSString *) ABRecordCopyValue(recordRef, kABPersonLastNameProperty);
-    NSString *fullName = [NSString stringWithFormat:@"%@ %@", firstName, lastName];
-    
-    ABMultiValueRef emailMultiValue = (ABMultiValueRef *) ABRecordCopyValue(recordRef, kABPersonEmailProperty);
-    NSArray *emailList = (NSArray *) ABMultiValueCopyArrayOfAllValues(emailMultiValue);
-    
-    if (!email && [emailList count] > 0) {
-        email = (NSString *) [emailList objectAtIndex:0];
-    }
-    
     BOOL addPerson = YES;
     
-    for(Participant *p in travel.participants) {
-        if ([p.name isEqualToString:fullName]) {
-            addPerson= NO;
-            break;
-        }
-    }
-
-    if (addPerson) {
-        person.travel = travel;
-        person.name = fullName;
-        person.email = email;
-        
-        UIImage *image = nil;
-        if(ABPersonHasImageData(recordRef)) {
-            
-            NSData *imgData = nil;
-            if (&ABPersonCopyImageDataWithFormat != nil) {
-                // iOS >= 4.1
-                imgData = (NSData *) ABPersonCopyImageDataWithFormat(recordRef, kABPersonImageFormatThumbnail);
-            } else {
-                // iOS < 4.1
-                imgData = (NSData *) ABPersonCopyImageData(recordRef);
-            }
-            
-            image = [[ImageCache instance] getImage:imgData];
-            
-            if (image) {
-                person.image = UIImagePNGRepresentation(image);
-            }
-            [imgData release];
-            
-        } else {
-            person.image = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"noImage" ofType:@"png"]];
-        }
-        
-        // get thumbnail
-        person.imageSmall = [Participant createThumbnail:person.image];
+    NSString *firstName = (NSString *) ABRecordCopyValue(recordRef, kABPersonFirstNameProperty);
+    NSString *lastName = (NSString *) ABRecordCopyValue(recordRef, kABPersonLastNameProperty);
+    
+    NSString *fullName = nil;
+    if (firstName && lastName) {
+        fullName = [NSString stringWithFormat:@"%@ %@", firstName, lastName];
+    } else if (firstName) {
+        fullName = firstName;
+    } else if (lastName) {
+        fullName = lastName;
+    } else {
+        addPerson = NO;
     }
     
-    CFRelease(emailMultiValue);
-    [emailList release];
-    [firstName release];
-    [lastName release];
+    if (fullName) {
+        ABMultiValueRef emailMultiValue = (ABMultiValueRef *) ABRecordCopyValue(recordRef, kABPersonEmailProperty);
+        NSArray *emailList = (NSArray *) ABMultiValueCopyArrayOfAllValues(emailMultiValue);
+        
+        if (!email && [emailList count] > 0) {
+            email = (NSString *) [emailList objectAtIndex:0];
+        }
+                
+        for(Participant *p in travel.participants) {
+            if ([p.name isEqualToString:fullName]) {
+                addPerson= NO;
+                break;
+            }
+        }
+        
+        if (addPerson) {
+            person.travel = travel;
+            person.name = fullName;
+            person.email = email;
+            
+            UIImage *image = nil;
+            if(ABPersonHasImageData(recordRef)) {
+                
+                NSData *imgData = nil;
+                if (&ABPersonCopyImageDataWithFormat != nil) {
+                    // iOS >= 4.1
+                    imgData = (NSData *) ABPersonCopyImageDataWithFormat(recordRef, kABPersonImageFormatThumbnail);
+                } else {
+                    // iOS < 4.1
+                    imgData = (NSData *) ABPersonCopyImageData(recordRef);
+                }
+                
+                image = [[ImageCache instance] getImage:imgData];
+                
+                if (image) {
+                    person.image = UIImagePNGRepresentation(image);
+                }
+                [imgData release];
+                
+            } else {
+                person.image = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"noImage" ofType:@"png"]];
+            }
+            
+            // get thumbnail
+            person.imageSmall = [Participant createThumbnail:person.image];
+        }
+        
+        CFRelease(emailMultiValue);
+        [emailList release];
+        [firstName release];
+        [lastName release];
+    }
     
     return addPerson;
 }
